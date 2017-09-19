09b4f5a299295ed60e1a420760360ac4841b4d3b
四月 <1586246693@qq.com>
2017-09-19 23:59:12 +0800

    修改错误 加强

--- src/main/java/com/mapper/Useperson.java
+++ src/main/java/com/mapper/Useperson.java
@@ -2,13 +2,29 @@ package com.mapper;
 
 
 import com.bean.Bean;
-
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import java.util.List;
+import java.util.Map;
 
 public interface Useperson {
 
-//    public List<Bean> all() throws Exception;
     public List<Bean> all() throws Exception;
 
+    public List<String> selectone(String name);
 
+    public List<String> selecttwo(Map<String ,Object> map);
+
+    public List<String> select(@Param("name")String name,@Param("sex") String sex);
+
+    public List<String> selectBean(@Param("be") Bean bean);
+
+    public List<String> selectbean(Bean bean);
+
+    public int into(Bean bean);
+
+    public int update(@Param("oldname") String oldname,
+                      @Param("newname")String newname);
+
+    public int delete(String name);
 }
--- src/main/test/Test.java
+++ src/main/test/Test.java
@@ -5,7 +5,9 @@ import com.mapper.Useperson;
 import com.sqlsession.GetSqlsession;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
@@ -16,6 +18,49 @@ public class Test {
             for (Bean p : person){
                 System.out.println(p);
             }
+
+            List<String> name = mapper.selectone("小红");
+            System.out.println(name);
+
+            Map<String ,Object> m = new HashMap<String, Object>();
+            m.put("name","小明");
+            m.put("sex","男");
+            name = mapper.selecttwo(m);
+            System.out.println(name);
+
+            name = mapper.select("小明","男");
+            System.out.println(name);
+
+            Bean bean = new Bean();
+            bean.setName("小明");
+            bean.setSex("男");
+            name = mapper.selectBean(bean);
+            System.out.println(name);
+
+            name = mapper.selectbean(bean);
+            System.out.println(name);
+
+
+//            添加
+//            bean.setName("小飞");
+//            bean.setSex("男");
+//            int i = mapper.into(bean);
+//            System.out.println(i);
+
+//            修改
+//            i = mapper.update("小飞","小马");
+//            System.out.println(i);
+
+//            删除
+//
+            int i = mapper.delete("小马");
+            System.out.println(i);
+
+             person= mapper.all();
+            for (Bean p : person){
+                System.out.println(p);
+            }
+
             sqlSession.commit();
         } catch (Exception e) {
             e.printStackTrace();
